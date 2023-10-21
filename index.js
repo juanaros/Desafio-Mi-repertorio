e4b96d35e81ac8266714c6fb62b13aa90bd34b05
Juan Aros <juaneduaros@gmail.com>
2023-10-21 19:31:46 -0300

    app funcionando

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ const app = express();
 
 const PORT =  3000;
 app.listen(PORT, console.log(`Servidor Up ${PORT}`));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+    console.log(__dirname);
+});
+
+app.get("/canciones", (req, res) => {
+    const canciones = JSON.parse(fs.readFileSync("repertorios.json", "utf8"));
+    res.json(canciones);
+});
+
+app.post("/canciones", (req, res) => {
+    const nuevaCancion = req.body;
+    const canciones = JSON.parse(fs.readFileSync("repertorios.json", "utf8"));
+    canciones.push(nuevaCancion);
+    fs.writeFileSync("repertorios.json", JSON.stringify(canciones));
+    res.send("Cancion agregada exitosamente");
+});
+
